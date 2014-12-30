50cf3eba3e34b526d4ca843ed635fa4585ba5adb
Julien Adam <julien.adam@rfq-hub.com>
2014-12-30 11:27:02 +0100

    Fixed saving, now saves working directory

--- AttachR/Models/DebuggingTarget.cs
+++ AttachR/Models/DebuggingTarget.cs
@@ -8,6 +8,7 @@ namespace AttachR.Models
         public bool Selected { get; set; }
         public string Executable { get; set; }
         public string CommandLineArguments { get; set; }
+        public string WorkingDirectory { get; set; }
         public TimeSpan Delay { get; set; }
         public List<string> SelectedDebuggingEngines { get; set; }
     }
