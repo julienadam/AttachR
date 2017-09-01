9da710fea6867133640a4fc8058d7555520dd484
Julien Adam <julien.adam@itg.com>
2017-09-01 10:50:45 +0200

    Added paket references for WiX tools
    hopefully fixing the packaging of the msi

--- BuildMsi.cmd
+++ BuildMsi.cmd
@@ -5,7 +5,7 @@ del installer\*.wixpdb
 del installer\*.msi
 del installer\attachrfiles.wxs
 pushd installer
-..\packages\ParaffinBundler.3.61\paraffin.exe -dir ..\AttachR\bin\release -groupName AttachRFiles attachrfiles.wxs -ext pdb -ext xml -ext exe -regexexclude .*\.vshost\..* -norootdirectory
-..\packages\WiX.Toolset.3.9.1208.0\tools\wix\candle.exe *.wxs
-..\packages\WiX.Toolset.3.9.1208.0\tools\wix\light.exe -ext WixNetFxExtension -ext WixUIExtension *.wixobj -o AttachR.msi
+..\packages\ParaffinBundler\paraffin.exe -dir ..\AttachR\bin\release -groupName AttachRFiles attachrfiles.wxs -ext pdb -ext xml -ext exe -regexexclude .*\.vshost\..* -norootdirectory
+..\packages\WiX.Toolset\tools\wix\candle.exe *.wxs
+..\packages\WiX.Toolset\tools\wix\light.exe -ext WixNetFxExtension -ext WixUIExtension *.wixobj -o AttachR.msi
 popd
